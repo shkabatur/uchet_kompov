7e9f549a4617e5ec93a2efe64f70c49f3cef105d
Denis Shkabatur <denis6705@gmail.com>
2020-01-14 19:21:03 +0300

    doing some progress

--- mysite/uchet/models.py
+++ mysite/uchet/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class RAM(models.Model):
+    physical_size = models.CharField(max_length=20) #SODIMM
+    amount = models.IntegerField(default=0)         #8gb
+    ram_type = models.CharField(max_length=20)      #ddr4
+    speed = models.IntegerField(default=0)          #2400
+
+class CPU(models.Model):
+    socket = models.CharField(max_length=20) 
+    chipset = models.CharField(max_length=20)
+    freequency = models.IntegerField(default=0)
+
+class HDD(models.Model):
+    hdd_type = models.CharField(max_length=20) 
+    capacity = models.IntegerField(default=0)
+    speed = models.IntegerField(default=0)
+
+class Computer(models.Model):
+    ram = models.ForeignKey(RAM, on_delete=models.CASCADE)
+    cpu = models.ForeignKey(CPU, on_delete=models.CASCADE)
+    HDD = models.ForeignKey(HDD, on_delete=models.CASCADE)
+    
