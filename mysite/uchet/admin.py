83cb00ce738d89f0765535ba5935366c20e1c745
Denis Shkabatur <denis6705@gmail.com>
2020-01-17 09:36:26 +0300

    add new fields

--- mysite/uchet/admin.py
+++ mysite/uchet/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 from .models import Computer, CPU, RAM, HDD
 # Register your models here.
 
-admin.site.register(Computer)
-admin.site.register(RAM)
-admin.site.register(HDD)
-admin.site.register(CPU)
+class cpuInline(admin.TabularInline):
+    model = CPU
+
+class ramInline(admin.TabularInline):
+    model = RAM
+
+class hddInline(admin.TabularInline):
+    model = HDD
+
+@admin.register(Computer)
+class ComputerAdmin(admin.ModelAdmin):
+    inlines = [cpuInline, ramInline, hddInline]
+
+#admin.site.register(RAM)
+#admin.site.register(HDD)
+#admin.site.register(CPU)
--- mysite/uchet/models.py
+++ mysite/uchet/models.py
@@ -2,13 +2,31 @@ from django.db import models
 
 # Create your models here.
 
+
+class Computer(models.Model):
+    inventory_number = models.CharField(max_length=20, default="",verbose_name="Инвентарный номер")
+    manufacturer = models.CharField(max_length=20, default="",verbose_name="Производитель") 
+    owner = models.CharField(max_length=20, default="",verbose_name="Материально ответственный")
+    last_service_date = models.DateTimeField(verbose_name="Дата последней проверки")
+    status = models.BooleanField(default=True, verbose_name="Иправность") 
+
+    def __str__(self):
+        return "%s %s %s" % (self.manufacturer, self.owner, self.inventory_number)
+
+    class Meta:
+        verbose_name = "Компьютер"
+        verbose_name_plural = "Компьютеры"
+        
+
 class RAM(models.Model):
+    sn =  models.CharField(max_length=20, default="", verbose_name="Серийный номер")
     manufacturer = models.CharField(max_length=20, default="", verbose_name="Производитель") 
-    physical_size = models.CharField(max_length=20, verbose_name="Фромфактор") 
-    amount = models.IntegerField(default=0, verbose_name="Размер")        
-    ram_type = models.CharField(max_length=20, verbose_name="Тип",)      
-    speed = models.IntegerField(default=0, verbose_name="Скорость",)  
-    status = models.BooleanField(default=True, verbose_name="Иправность")    
+    physical_size = models.CharField(max_length=8, verbose_name="Фромфактор") 
+    amount = models.IntegerField(default=0, verbose_name="Размер (Гб)")        
+    ram_type = models.CharField(max_length=6, verbose_name="Тип",)      
+    speed = models.IntegerField(default=0, verbose_name="Скорость (Ггц)",)  
+    status = models.BooleanField(default=True, verbose_name="Иправность")
+    computer = models.ForeignKey(Computer, on_delete=models.CASCADE, verbose_name="Компьютер")
 
     def __str__(self):
         return "%s %s %dgb %dGHz" % (self.manufacturer, self.ram_type, self.amount, self.speed)
@@ -18,11 +36,13 @@ class RAM(models.Model):
         verbose_name_plural = "Оперативная память"
 
 class CPU(models.Model):
+    sn =  models.CharField(max_length=20, default="", verbose_name="Серийный номер")
     manufacturer = models.CharField(max_length=20, default="", verbose_name="Производитель")
-    socket = models.CharField(max_length=20, verbose_name="Сокет") 
+    socket = models.CharField(max_length=8, verbose_name="Сокет") 
     freequency = models.IntegerField(default=0, verbose_name="Частота")
     cores = models.IntegerField(default=0, verbose_name="Количество ядер")
     status = models.BooleanField(default=True, verbose_name="Иправность") 
+    computer = models.ForeignKey(Computer, on_delete=models.CASCADE, verbose_name="Компьютер")
 
     def __str__(self):
         return "%s %s %dGHz cores: %d " % (self.manufacturer, self.socket, self.freequency, self.cores)
@@ -32,11 +52,13 @@ class CPU(models.Model):
         verbose_name_plural = "Процессоры"
 
 class HDD(models.Model):
+    sn =  models.CharField(max_length=20, default="", verbose_name="Серийный номер")
     manufacturer = models.CharField(max_length=20, default="",verbose_name="Производитель") 
-    hdd_type = models.CharField(max_length=20, verbose_name="Фромфактор") 
-    capacity = models.IntegerField(default=0, verbose_name="Объём")
+    hdd_type = models.CharField(max_length=6, verbose_name="Фромфактор") 
+    capacity = models.IntegerField(default=0, verbose_name="Объём (Гб)")
     speed = models.IntegerField(default=0, verbose_name="Скорость")
     status = models.BooleanField(default=True, verbose_name="Иправность")
+    computer = models.ForeignKey(Computer, on_delete=models.CASCADE, verbose_name="Компьютер")
 
     def __str__(self):
         return "%s %s %dgb %d" % (self.manufacturer, self.hdd_type, self.capacity, self.speed)
@@ -46,21 +68,5 @@ class HDD(models.Model):
         verbose_name_plural = "Жесткие диски"
     
 
-class Computer(models.Model):
-    inventory_number = models.CharField(max_length=20, default="",verbose_name="Инвентарный номер")
-    manufacturer = models.CharField(max_length=20, default="",verbose_name="Производитель") 
-    owner = models.CharField(max_length=20, default="",verbose_name="Материально ответственный")
-    ram = models.ForeignKey(RAM, on_delete=models.CASCADE, verbose_name="Оперативная память")
-    cpu = models.ForeignKey(CPU, on_delete=models.CASCADE, verbose_name="Процессор")
-    hdd = models.ForeignKey(HDD, on_delete=models.CASCADE, verbose_name="Жесткий диск",)
-    last_service_date = models.DateTimeField(verbose_name="Дата последней проверки")
-    status = models.BooleanField(default=True, verbose_name="Иправность") 
 
-    def __str__(self):
-        return "%s %s %s %s" % (self.manufacturer, self.owner, self.cpu, self.inventory_number)
-
-    class Meta:
-        verbose_name = "Компьютер"
-        verbose_name_plural = "Компьютеры"
-        
     
